8621e0ac80866aed46b511c78c86ac62f3b80f1b
Aramis jaime <you@example.com>
2024-03-10 03:46:32 -0300

    responsive end

--- src/components/postCard/postCard.tsx
+++ src/components/postCard/postCard.tsx
@@ -8,34 +8,40 @@ interface Props {
 
 export default function PostCard({ postDetail }: Props) {
   const [styleSize, setSyleSize] = useState<string>(s.size1);
+  // const [multiply, setMultiply] = useState<number>(1);
 
   useEffect(() => {
     switch (postDetail.size.size) {
       case 1:
         setSyleSize(s.size1);
+        // setMultiply(0.3);
         break;
       case 2:
         setSyleSize(s.size2);
+        // setMultiply(0.35);
+
         break;
       case 3:
         setSyleSize(s.size3);
+        // setMultiply(0.4);
         break;
       case 4:
         setSyleSize(s.size4);
+        // setMultiply(0.6);
         break;
       default:
         setSyleSize(s.size1);
     }
   }, [postDetail.size.size]);
   return (
-    <div className={styleSize}>
-      <h3 style={{ color: "white" }}>
+    <div
+      className={styleSize}
+      // style={{ width: window.innerWidth * 0.8 * multiply }}
+    >
+      {/* <h3 style={{ color: "white" }}>
         {postDetail.importance.importance} {postDetail.size.size}
-      </h3>
+      </h3> */}
       <img src={postDetail.img} alt="image" className={styleSize} />
-      {/* <h6>{postDetail.title}</h6>
-      <p className={s.descrption}>{postDetail.description}</p>
-      <p className={s.phone}>{postDetail.number_phone}</p> */}
     </div>
   );
 }
