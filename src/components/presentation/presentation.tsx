9f2586a4d2ff1a2b2e9b93c39557d9034a4955dc
Aramis jaime <you@example.com>
2024-05-23 18:13:37 -0300

    add share

--- src/components/presentation/presentation.tsx
+++ src/components/presentation/presentation.tsx
@@ -2,9 +2,17 @@ import s from "./presentations.module.scss";
 import imgBk from "../../assets/images/bk.jpg";
 import { Link } from "react-router-dom";
 import { useContextAuth } from "../../contexts/hooks/useContextAuth";
+import shareicon from "../../assets/icons/share.svg";
 
 export default function Presentation() {
   const contextAuth = useContextAuth();
+
+  const handlerShareSite = () => {
+    navigator.share({
+      text: "Busca lo que necesitas en Parque Patriciois",
+      url: `${window.origin}`,
+    });
+  };
 
   return (
     <div
@@ -22,9 +30,14 @@ export default function Presentation() {
           <br />
           {/* todo para hacer de nuestro barrio cada dia mejor... */}
         </p>
-        <Link to="/contact" className={s.button}>
-          PUBLICITE AQUI
-        </Link>
+        <div className={s.containerLinks}>
+          <button onClick={handlerShareSite} className={s.share}>
+            <img src={shareicon} alt="share" />
+          </button>
+          <Link to="/contact" className={s.button}>
+            PUBLICITE AQUI
+          </Link>
+        </div>
       </div>
     </div>
   );
